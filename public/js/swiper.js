33afd443f88014bac97f4e119b64a986b4e47706
diorixo <m1240412@gmail.com>
2025-10-02 14:59:55 +0300

    index page remake

---
+++ public/js/swiper.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+            loop: true,
+            speed: 800,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            effect: 'fade',
+            fadeEffect: {
+                crossFade: true
+            }
+});
